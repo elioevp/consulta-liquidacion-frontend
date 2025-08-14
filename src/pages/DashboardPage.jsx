0f1325ecfc8eec517fc00fbf655e93a867ebab8a
Elio Villalobos <elioevp@gmail.com>
2025-08-14 01:06:20 -0400

    Hardcoded URL backend in frontend dashboardPage.jsx

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -23,7 +23,7 @@ const DashboardPage = () => {
         setReportData(null);
 
         try {
-                                               const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+                                               const apiUrl = import.meta.env.VITE_API_URL || 'https://report-backend.azurewebsites.net/api' || 'http://localhost:3000/api';
             const response = await axios.get(`${apiUrl}/report`, {
                 params: {
                     username: username,
